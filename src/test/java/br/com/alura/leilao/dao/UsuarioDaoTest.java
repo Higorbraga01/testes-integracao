3aea6e1afb52e3a62a0ac5c86bbebb30c3cc9f33
Higorbraga01 <higorbraga00@gmail.com>
2021-10-11 11:26:14 -0300

    Refatora codigo de testes para  melhor manutencao

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -3,29 +3,50 @@ package br.com.alura.leilao.dao;
 import br.com.alura.leilao.model.Usuario;
 import br.com.alura.leilao.util.JpaUtil;
 import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UsuarioDaoTest {
 
     private UsuarioDao dao;
+    private  EntityManager em ;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.em = JpaUtil.getEntityManger();
+        this.dao = new UsuarioDao(em);
+        em.getTransaction().begin();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        em.getTransaction().rollback();
+    }
 
     @Test
-    void testeBuscarUsuarioPorUserName() {
-        EntityManager em = JpaUtil.getEntityManger();
-        this.dao = new UsuarioDao(em);
-        Usuario usuario = new Usuario("Fulano", "fulano@email.comn", "123456");
-        em.getTransaction().begin();
-        em.persist(usuario);
-        em.getTransaction().commit();
-
+    void deveriaEncontrarUsuarioCadastrado() {
+        Usuario usuario = criarUsuario();
         Usuario encontrado = this.dao.buscarPorUsername(usuario.getNome());
         Assert.assertNotNull(encontrado);
     }
 
+    @Test
+    void naoDeveriaEncontrarUsuarioCadastrado() {
+        criarUsuario();
+        Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("Beltrano"));
+    }
+
+    private  Usuario criarUsuario() {
+        Usuario usuario = new Usuario("Fulano", "fulano@email.comn", "123456");
+        em.persist(usuario);
+        return usuario;
+    }
     @Test
     void deletar() {
     }
