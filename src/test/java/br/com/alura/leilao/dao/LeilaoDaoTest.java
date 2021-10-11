16723d90ddb356366121d8907601a6052df239e7
Higorbraga01 <higorbraga00@gmail.com>
2021-10-11 12:44:13 -0300

    Testando metodos de insert e updaytte da clase Leilao

---
+++ src/test/java/br/com/alura/leilao/dao/LeilaoDaoTest.java
@@ -0,0 +1,66 @@
+package br.com.alura.leilao.dao;
+
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.util.JpaUtil;
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeilaoDaoTest {
+
+    private LeilaoDao dao;
+    private EntityManager em ;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.em = JpaUtil.getEntityManger();
+        this.dao = new LeilaoDao(em);
+        em.getTransaction().begin();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        em.getTransaction().rollback();
+    }
+
+    @Test
+    public void deveriaCadastrarUmLeilao(){
+        Usuario usuario = criarUsuario();
+        Leilao leilao = new Leilao("Mochila", new BigDecimal("70"), LocalDate.now(), usuario);
+        leilao = dao.salvar(leilao);
+
+        Leilao salvo = dao.buscarPorId(leilao.getId());
+        Assert.assertNotNull(salvo);
+    }
+
+
+    @Test
+    public void deveriaAtualizarUmLeilao(){
+        Usuario usuario = criarUsuario();
+        Leilao leilao = new Leilao("Mochila", new BigDecimal("70"), LocalDate.now(), usuario);
+        leilao = dao.salvar(leilao);
+        leilao.setNome("Celular");
+        leilao.setValorInicial(new BigDecimal("400"));
+        leilao = dao.salvar(leilao);
+
+        Leilao salvo = dao.buscarPorId(leilao.getId());
+        Assert.assertEquals("Celular", salvo.getNome());
+        Assert.assertEquals(new BigDecimal("400"), salvo.getValorInicial());
+    }
+
+    private Usuario criarUsuario() {
+        Usuario usuario = new Usuario("Fulano", "fulano@email.comn", "123456");
+        em.persist(usuario);
+        return usuario;
+    }
+
+}
